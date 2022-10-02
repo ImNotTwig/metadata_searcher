6245065cb0272b693d1931aa1aaad6279c1f4d20
ImNotTwig <ImNotTwig@gmail.com>
2022-10-01 19:59:39 -0500

    made it where you can input multiple artists after you run the program

--- src/main.rs
+++ src/main.rs
@@ -40,112 +40,139 @@ pub struct ArtistData {
 
 fn main() {
 
-//////////////////getting the artist mbid and name//////////////////////////////////////////////
-    
-    let artist_query = "Lorde".to_string();
-    let artist_mbid = get_artist_mbid(&artist_query);
+//////////////////Getting artists from the user to search the metadata for/////////////////////
+
+    let mut artist = "a".to_string();
+    let mut artists_list = vec![];
+    let mut counter = 0;
+    while artist.chars().take(1).last().unwrap() != '\n'  {
+        if counter == 0 {
+            println!("which artists would you like to query?");
+            println!("When done typing full name of artist press enter.");
+            println!("If you dont want to query anymore artists press enter without putting an artist");
+            counter = 1;
+        }else {
+            println!("");
+        }
+        artist = String::new();
+
+        std::io::stdin().read_line(&mut artist).unwrap();
+
+        if artist.chars().take(1).last().unwrap() == '\n' {
+            break
+        }
+        artists_list.push(artist.clone());
+    }
+
+    for artist in artists_list {
+
+//////////////////Getting the artist mbid and name//////////////////////////////////////////////
+        
+        let artist_query = artist;
+        let artist_mbid = get_artist_mbid(&artist_query);
 
 //////////////////getting info about the artist from musicbrainz////////////////////////////////
 
-    let artist_info = Artist::fetch()
-        .id(&artist_mbid.as_ref().unwrap().to_string())
-        .with_release_groups()
-        .execute()
-        .unwrap();
-        
-//////////////////getting mbids for every release group/////////////////////////////////////////
- 
-    let mut release_group_mbids = vec![];
-
-    for release_group in artist_info.release_groups.unwrap() {
-        release_group_mbids.push(release_group.id);
-    }
-
-//////////////////getting mbids for every release///////////////////////////////////////////////
-
-    let mut release_mbids = vec![];
-
-    for mbid in &release_group_mbids {
-        let release_group_info = &mut ReleaseGroup::fetch()
-            .id(&mbid)
-            .with_releases()
-            .execute()
-            .unwrap();
-        
-        release_mbids.push((release_group_info.releases.as_ref().unwrap()[0].id.clone(), 
-                            release_group_info.primary_type.as_ref().unwrap().to_string()));
-    }
-
-//////////////////getting data from the releases to make ReleaseData////////////////////////////
-
-    let mut releases_hashmap = HashMap::new();
-
-    for mbid in release_mbids { //for release in releases
-        let mut song_hashmap = HashMap::new();
-        let release = &mut Release::fetch()
-            .id(&mbid.0)
-            .with_recordings()
+        let artist_info = Artist::fetch()
+            .id(&artist_mbid.as_ref().unwrap().to_string())
+            .with_release_groups()
             .execute()
             .unwrap();
 
-        for song in release.media.as_ref().unwrap()[0].tracks.as_ref().unwrap() { //for every track make a SongData struct
-            let song_data = SongData{ mbid:song.id.clone(), number:song.position as i32};
-            song_hashmap.insert(song.title.clone(), song_data);
+//////////////////Getting mbids for every release group/////////////////////////////////////////
         
+        let mut release_group_mbids = vec![];
+
+        for release_group in artist_info.release_groups.unwrap() {
+            release_group_mbids.push(release_group.id);
         }
 
-        let release_data = ReleaseData {
-            mbid: mbid.0,
-            type_of_release: mbid.1,
-            tracks: song_hashmap,
-        };
+//////////////////Getting mbids for every release///////////////////////////////////////////////
 
-        let title_of_release = format!("{} - {}", release.title.clone(), release_data.type_of_release); 
+        let mut release_mbids = vec![];
 
-        releases_hashmap.insert(title_of_release, release_data);
-    }
+        for mbid in &release_group_mbids {
+            let release_group_info = &mut ReleaseGroup::fetch()
+                .id(&mbid)
+                .with_releases()
+                .execute()
+                .unwrap();
+
+            release_mbids.push((release_group_info.releases.as_ref().unwrap()[0].id.clone(), 
+                                release_group_info.primary_type.as_ref().unwrap().to_string()));
+        }
+
+//////////////////Getting data from the releases to make ReleaseData////////////////////////////
+
+        let mut releases_hashmap = HashMap::new();
+
+        for mbid in release_mbids { //for release in releases
+            let mut song_hashmap = HashMap::new();
+            let release = &mut Release::fetch()
+                .id(&mbid.0)
+                .with_recordings()
+                .execute()
+                .unwrap();
+
+            for song in release.media.as_ref().unwrap()[0].tracks.as_ref().unwrap() { //for every track make a SongData struct
+                let song_data = SongData{ mbid:song.id.clone(), number:song.position as i32};
+                song_hashmap.insert(song.title.clone(), song_data);
+            
+            }
+
+            let release_data = ReleaseData {
+                mbid: mbid.0,
+                type_of_release: mbid.1,
+                tracks: song_hashmap,
+            };
+
+            let title_of_release = format!("{} - {}", release.title.clone(), release_data.type_of_release); 
+
+            releases_hashmap.insert(title_of_release, release_data);
+        }
 
 //////////////////Creating ArtistData///////////////////////////////////////////////////////////
 
-    let artist_data = ArtistData {
-        mbid: artist_mbid.as_ref().unwrap().to_string(),
-        artist_type: artist_info.artist_type.as_ref().unwrap().to_string(),
-        disambiguation: artist_info.disambiguation.clone(),
-        releases: releases_hashmap
-    };
+        let artist_data = ArtistData {
+            mbid: artist_mbid.as_ref().unwrap().to_string(),
+            artist_type: artist_info.artist_type.as_ref().unwrap().to_string(),
+            disambiguation: artist_info.disambiguation.clone(),
+            releases: releases_hashmap
+        };
 
-    let mut artist_name = String::new();
-    
-    if artist_info.disambiguation.clone() != "" {
-        artist_name = format!("{} - {}", artist_info.name, artist_info.disambiguation);
-    
-    }else {
-        artist_name = artist_info.name;
-    }
+        let mut artist_name = String::new();
+
+        if artist_info.disambiguation.clone() != "" {
+            artist_name = format!("{} - {}", artist_info.name, artist_info.disambiguation);
+        
+        }else {
+            artist_name = artist_info.name;
+        }
 
 //////////////////Converting the ArtistData to a Json and uploading it to a file////////////////
 
-    let file_path = Path::new("./data.json");
+        let file_path = Path::new("./data.json");
 
-    let file_data = fs::read_to_string(file_path).unwrap();
+        let file_data = fs::read_to_string(file_path).unwrap();
 
-    let json: serde_json::Value = serde_json::from_str(&file_data).unwrap();
+        let json: serde_json::Value = serde_json::from_str(&file_data).unwrap();
 
-    let mut json = to_hashmap(json);
+        let mut json = to_hashmap(json);
 
-    json.insert(artist_name, artist_data);
+        json.insert(artist_name, artist_data);
 
-    let buf = Vec::new();
-    let formatter = serde_json::ser::PrettyFormatter::with_indent(b"    ");
-    let mut ser = serde_json::Serializer::with_formatter(buf, formatter);
-    json.serialize(&mut ser).unwrap();
+        let buf = Vec::new();
+        let formatter = serde_json::ser::PrettyFormatter::with_indent(b"    ");
+        let mut ser = serde_json::Serializer::with_formatter(buf, formatter);
+        json.serialize(&mut ser).unwrap();
 
-    let mut file = File::create("./data.json").unwrap();
+        let mut file = File::create("./data.json").unwrap();
 
-    write!(file, "{}", String::from_utf8(ser.into_inner()).unwrap()).unwrap();
+        write!(file, "{}", String::from_utf8(ser.into_inner()).unwrap()).unwrap();
+    }
 }
 
-//////////////////function to get the artists mbid//////////////////////////////////////////////
+//////////////////Function to get the artists mbid//////////////////////////////////////////////
 
 fn get_artist_mbid(artist_name: &String) -> Option<String> {
     
